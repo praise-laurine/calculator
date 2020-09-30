f96391566af6df68d1c50ea245114043203468db
praise-laurine <praiselaurine10@gmail.com>
2020-10-01 01:38:44 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var add =function(number1, number2) {
+    return number1 + number2
+};
+add(5,4);
+alert(add(5,4));
+
+var subtract =function(number1, number2) {
+    return number1 - number2
+};
+subtract(10,5);
+alert(subtract(10,5));
+
+var multiply =function(number1, number2) {
+    return number1 * number2
+};
+multiply(5,6);
+alert(multiply(5,6));
+
+var divide =function(number1, number2) {
+    return number1 / number2
+};
+divide(60,10);
+alert(divide(60,10));
+
+var add =function(number1, number2) {
+    return number1 + number2
+};
+var number1 =parseInt (prompt("Entera number:"));
+var number2 =parseInt (prompt("Enter another number:"));
+alert(add(number1,number2));
+function makeNoise(animalSound) {
+    alert(animalSound);
+}
+makeNoise("Woof!");
+function subtract(firstNumber, secondNumber){
+    return firstNumber - secondNumber ;
+};
